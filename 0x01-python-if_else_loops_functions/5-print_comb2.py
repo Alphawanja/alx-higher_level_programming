d86ab49162b835fa32a1768ae9f8367bb52c77b0
Alphawanja <alphamariegarze823@gmail.com>
2023-01-04 10:56:16 -0800

    python functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-    print("{:0>2d}".format(x), end=", ")
-
-    print(x+1)
+for x in range(0, 100):
+    if x == 99:
+    print("{}".format(x))
+else:
+    print("{:02}".format(x), end=", ")
