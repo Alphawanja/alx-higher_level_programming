36ec3bca25ab7b0272c72ae66f0befa6c9b70b35
Alphawanja <alphamariegarze823@gmail.com>
2023-01-24 12:40:17 -0800

    classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Class Square that defines a square by:
     Private instance attribute: size
-    Instantiation with size (no  type/value verification)
+    Instantiation with size (no type/value verification)
 """
 
 class Square:
