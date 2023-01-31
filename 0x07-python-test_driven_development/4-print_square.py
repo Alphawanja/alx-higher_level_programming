42ff6efd17e0b16d184faf39870df595f61ffa30
Alphawanja <alphamariegarze823@gmail.com>
2023-01-31 10:50:39 -0800

    tests

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module is composed by a function that prints a square with the character '#'
+"""
+
+
+def print_square(size):
+    """ Function that prints a square with the character '#'
+    Args:
+        
