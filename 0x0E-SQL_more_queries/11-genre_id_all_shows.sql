7751e6c735a2d1c478986a6553b5e2d161bd766d
Alphawanja <alphamariegarze823@gmail.com>
2023-03-15 14:31:20 -0700

    sql queries

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- List all shows contained in the database hbtn_0d_tvshows
 -- Query to perform operation
-SELECT sh.title, shgr.genre_id FROM tv_shows AS sh
-	LEFT JOIN tv_show_genres AS shgr ON sh.id=shgr.show.show_id
-	ORDER BY sh.title, shgr.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
