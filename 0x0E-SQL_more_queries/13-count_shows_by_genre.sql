de2bbb5a5ae1c46cec7d30ae26ee5b9a4cc4fad5
Alphawanja <alphamariegarze823@gmail.com>
2023-03-15 14:37:48 -0700

    sql queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- Lists all shows contained in hbtn-0d_tvshows without a genre linked
 -- Query to perform operation
 SELECT gr.name AS genre,
-	COUNT(shgr.genre_id) AS number_of shows FROM tv_genres AS gr
-	JOIN tv_show_genres AS shgr
-	ON gr.id=shgr.genre_id
-	GROUP BY shgr.genre_id
-	ORDER BY number_of_shows DESC;
+COUNT(tv_show_genres.genre.id) AS number_of shows FROM tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC, tv_genres.id ASC;
